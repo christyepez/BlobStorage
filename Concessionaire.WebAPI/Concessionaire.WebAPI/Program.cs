01a2da4fc9242a4df79b144af2103d98eef92f45
Christian Yepez <christyepez@gmail.com>
2025-02-12 16:08:58 -0500

    ajuste programa

--- Concessionaire.WebAPI/Concessionaire.WebAPI/Program.cs
+++ Concessionaire.WebAPI/Concessionaire.WebAPI/Program.cs
@@ -38,12 +38,12 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseCors(options =>
